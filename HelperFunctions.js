8017b2d4624b4c283be833789f88eb4f685cca18
WWW <Willi_W@gmx.at>
2013-03-14 14:47:41 +0100

    add selectItemByValue function

--- HelperFunctions.js
+++ HelperFunctions.js
@@ -50,3 +50,11 @@ function randBool(){
 		return false;
 	}
 }
+
+function selectItemByValue(elmnt, value){
+    for(var i=0; i < elmnt.options.length; i++)
+    {
+      if(elmnt.options[i].value == value)
+        elmnt.selectedIndex = i;
+    }
+}
